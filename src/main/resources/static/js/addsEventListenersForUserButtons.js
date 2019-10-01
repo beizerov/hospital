71ab626885c6b692c1bb632ae5d924956d00328f
Alexei Beizerov <serothim@gmail.com>
2019-10-01 22:52:16 +0300

    Refactoring JS source code.

--- src/main/resources/static/js/addsEventListenersForUserButtons.js
+++ src/main/resources/static/js/addsEventListenersForUserButtons.js
@@ -1,19 +1,24 @@
+'use strict';
+
+
 /**
  * Adds a event listeners for a user deletion buttons.
  */
 document.addEventListener('DOMContentLoaded', () => {
-	const buttons = document.getElementsByClassName('delete-btn');
-	for (const button of buttons)
-		button.addEventListener('mouseover', function() {
-			this.src = '/images/red-recycle-bin.png';
-		});
+  const buttons = document.getElementsByClassName('delete-btn');
+  for (const button of buttons) {
+    button.addEventListener('mouseover', function() {
+      button.src = '/images/red-recycle-bin.png';
+    });
+  }
 });
 document.addEventListener('DOMContentLoaded', () => {
-	const buttons = document.getElementsByClassName('delete-btn');
-	for (const button of buttons)
-		button.addEventListener('mouseout', function() {
-			this.src = '/images/green-recycle-bin.png';
-		});
+  const buttons = document.getElementsByClassName('delete-btn');
+  for (const button of buttons) {
+    button.addEventListener('mouseout', function() {
+      button.src = '/images/green-recycle-bin.png';
+    });
+  }
 });
 
 
@@ -21,16 +26,18 @@ document.addEventListener('DOMContentLoaded', () => {
  * Adds a event listeners for a user edit buttons.
  */
 document.addEventListener('DOMContentLoaded', () => {
-	const buttons = document.getElementsByClassName('edit-btn');
-	for (const button of buttons)
-		button.addEventListener('mouseover', function() {
-			this.src = '/images/pencil-green.png';
-		});
+  const buttons = document.getElementsByClassName('edit-btn');
+  for (const button of buttons) {
+    button.addEventListener('mouseover', function() {
+      button.src = '/images/pencil-green.png';
+    });
+  }
 });
 document.addEventListener('DOMContentLoaded', () => {
-	const buttons = document.getElementsByClassName('edit-btn');
-	for (const button of buttons)
-		button.addEventListener('mouseout', function() {
-			this.src = '/images/pencil-blue.png';
-		});
+  const buttons = document.getElementsByClassName('edit-btn');
+  for (const button of buttons) {
+    button.addEventListener('mouseout', function() {
+      button.src = '/images/pencil-blue.png';
+    });
+  }
 });
--- src/main/resources/static/js/showHidePasswordButton.js
+++ src/main/resources/static/js/showHidePasswordButton.js
@@ -1,26 +1,28 @@
+'use strict';
+
+
 /**
  * Show/hide password button on login page
  */
+$(function() {
+  $('.hide-show').show();
+  $('.hide-show span').addClass('show');
 
+  $('.hide-show span').click(function() {
+    if ($('.hide-show span').hasClass('show')) {
+      $('.hide-show span').text('Hide');
+      $('input[name="password"]').attr('type', 'text');
+      $('.hide-show span').removeClass('show');
+    } else {
+      $('.hide-show span').text('Show');
+      $('input[name="password"]').attr('type', 'password');
+      $('.hide-show span').addClass('show');
+    }
+  });
 
-$(function () {
-    $('.hide-show').show();
-    $('.hide-show span').addClass('show');
-
-    $('.hide-show span').click(function () {
-        if ($(this).hasClass('show')) {
-            $(this).text('Hide');
-            $('input[name="password"]').attr('type', 'text');
-            $(this).removeClass('show');
-        } else {
-            $(this).text('Show');
-            $('input[name="password"]').attr('type', 'password');
-            $(this).addClass('show');
-        }
-    });
-
-    $('form button[type="submit"]').on('click', function () {
-        $('.hide-show span').text('Show').addClass('show');
-        $('.hide-show').parent().find('input[name="password"]').attr('type', 'password');
-    });
+  $('form button[type="submit"]').on('click', function() {
+    $('.hide-show span').text('Show').addClass('show');
+    $('.hide-show').parent().find('input[name="password"]')
+        .attr('type', 'password');
+  });
 });
